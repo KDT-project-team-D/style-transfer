c2dd5214e39c2166c4d68eaa803013fa0c27a5d6
Bogeunk <95890510+Bogeunk@users.noreply.github.com>
2022-04-28 16:02:48 +0900

    Update urls.py

--- ai_project/urls.py
+++ ai_project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', Main.as_view(), name='main'),
+    path('', Main.as_view(), name='main'),
     path('content/', include('content.urls')),
 ]
 
